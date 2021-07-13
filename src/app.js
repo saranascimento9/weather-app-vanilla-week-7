49d133367fcb980d2f958b4d960dd95139b6b0e0
saranascimento9 <53081056+saranascimento9@users.noreply.github.com>
2021-07-13 20:53:02 +0100

    added the city variable

--- src/app.js
+++ src/app.js
@@ -27,7 +27,6 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
-
   let dateElement = document.querySelector("#date");
 
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
@@ -35,11 +34,11 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "5bceb710884f2fc43e7a158f3af93778";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
+let city = "Lisboa";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
